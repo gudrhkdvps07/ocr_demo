b29dff5dd1e7d15a0d2b5af4d3f18baf942affb0
gudrhkdvps07 <gudrhkdvps07@gmail.com>
2025-10-02 12:59:25 +0900

    전처리 테스트 + 이미지 띄우기

--- ocr_demo_cd.py
+++ ocr_demo_cd.py
@@ -10,49 +10,67 @@ path_img1 = "./meme.jpg"
 path_img2 = "./resume.png"
 path_img3 = "./test.png"
 
-# 테스트할 이미지들을 리스트로 관리 (OpenCV로 불러옴)
 images = [
     ("Image 1", cv2.imread(path_img1)),
     ("Image 2", cv2.imread(path_img2)),
     ("Image 3", cv2.imread(path_img3)),
 ]
 
-# PSM 모드 리스트
-psm_options = [3, 4, 6, 11]   # 문단형, 문장형, 블록형, 짧은 텍스트
+psm_options = [6]   
+oem_options = [3]   
 
-# OEM 모드 리스트 (1=LSTM, 3=base)
-oem_options = [1, 3]
+# 전처리 함수
+def preprocess_var(gray):
+    variants = {}
+    # 1. Gray
+    variants["Gray"] = gray
+    # 2. Otsu
+    _, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    variants["Otsu"] = otsu
+    # 3. Adaptive
+    adaptive = cv2.adaptiveThreshold(
+        gray, 255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY,
+        31, 15
+    )
+    variants["Adaptive"] = adaptive
+    # 4. CLAHE + Adaptive
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    clahe_img = clahe.apply(gray)
+    adaptive_clahe = cv2.adaptiveThreshold(
+        clahe_img, 255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY,
+        31, 15
+    )
+    variants["CLAHE+Adaptive"] = adaptive_clahe
+    return variants
 
+# 실행
 for name, img in images:
     print("=" * 70)
     print(f" {name} 결과")
 
-    # 1. Grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    variants = preprocess_var(gray)
 
-    # 2. CLAHE (대비 강화)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    gray = clahe.apply(gray)
+    for variant_name, proc_img in variants.items():
+        print("-" * 60)
+        print(f"[전처리: {variant_name}]")
 
-    # 4. Adaptive Threshold
-    thresh = cv2.adaptiveThreshold(
-        gray, #입력 이미지 = gray
-        255,  #최대값으로 설정
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY,
-        31, #블록 사이즈
-        10  # C
-    )
+        # OCR 실행
+        for psm in psm_options:
+            for oem in oem_options:
+                config = f'--oem {oem} --psm {psm} -c user_defined_dpi=300'
+                text = pytesseract.image_to_string(proc_img, lang="kor+eng", config=config)
+                print(f" OEM {oem} | PSM {psm} 결과:")
+                print(text.strip())
 
-    # OCR 실행 (PSM × OEM 조합)
-    for psm in psm_options:
-        for oem in oem_options:
-            config = f'--oem {oem} --psm {psm} -c user_defined_dpi=300'
+        # 이미지 창으로 띄우기
+        cv2.imshow(f"{name} - {variant_name}", proc_img)
 
-            text = pytesseract.image_to_string(thresh, lang="kor+eng", config=config)
+    print("\n")
 
-            print("-" * 60)
-            print(f" OEM {oem} | PSM {psm} 결과:")
-            print(text.strip())
-
-    print("\n")  # 이미지별 구분
+cv2.waitKey(0)   # 아무 키나 입력하면 닫히도록.
+cv2.destroyAllWindows()
