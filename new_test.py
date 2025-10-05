0ba304d6d3e90ae9df0869cc8f070fbee328730d
gudrhkdvps07 <gudrhkdvps07@gmail.com>
2025-10-05 22:43:22 +0900

    전처리 추가...

---
+++ new_test.py
@@ -0,0 +1,128 @@
+import pytesseract
+import cv2
+import numpy as np
+
+# Tesseract 실행 파일 경로
+tesseract_path = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = tesseract_path
+
+# 이미지 경로들
+path_img1 = "./meme.jpg"
+path_img2 = "./resume.png"
+path_img3 = "./test.png"
+
+images = [
+    ("Image 1", cv2.imread(path_img1)),
+    ("Image 2", cv2.imread(path_img2)),
+    ("Image 3", cv2.imread(path_img3)),
+]
+
+
+# OCR 옵션
+psm_options = [6]  
+oem_options = [3]   
+
+
+#함수 LIST
+def resize_img(img, name, max_len=1000):
+    h, w = img.shape[:2]
+    print(f" {name} 원본 크기: {w} x {h}")
+    if w > h:
+        new_w = max_len
+        new_h = int(h / w * max_len)
+    else:
+        new_h = max_len
+        new_w = int(w / h * max_len)
+    return cv2.resize(img, (new_w, new_h))
+
+def morph_gradient(gray, ksize=(3,3)):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, ksize)
+    return cv2.morphologyEx(gray, cv2.MORPH_GRADIENT, kernel)
+
+
+def adaptive_threshold(gray, block_size=25, C=10, invert=True):
+    th = cv2.adaptiveThreshold(
+        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY, block_size, C
+    )
+    if invert:
+        th = cv2.bitwise_not(th)
+    return th
+
+def morph_close(bin_img, ksize=(3, 3), iterations=1):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, ksize)
+    closed = cv2.morphologyEx(bin_img, cv2.MORPH_CLOSE, kernel, iterations=iterations)
+    return closed
+
+def find_contour(bin_img):
+    contours, _ = cv2.findContours(
+                                    bin_img, 
+                                    mode=cv2.RETR_EXTERNAL, #외곽선만 검출
+                                    method=cv2.CHAIN_APPROX_SIMPLE  #꼭짓점만 저장
+                                    )
+    return contours
+
+
+# 전처리 실행
+for name, img in images:
+    if img is None:
+        print(f"{name} 이미지를 불러올 수 없음.")
+        continue
+
+    print("=" * 70)
+    print(f" {name} 결과")
+
+    # 1. Resize
+    img_resized = resize_img(img, name)
+
+    # 2. Gray
+    gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
+
+    # 3. Morph Gradient
+    grad = morph_gradient(gray)
+
+    # 4. Adaptive Threshold
+    th = adaptive_threshold(grad, block_size=35, C=5)
+
+    # 5. Morph close
+    closed = morph_close(th, ksize=(3, 3), iterations=1)
+
+    # 6. Contour
+    contours = find_contour(closed)
+    contour_img = np.zeros_like(closed)
+    cv2.drawContours(contour_img, contours, -1, (255, 255, 255), 1)
+
+    # 전처리 관리
+    variants = {
+        "Gray": gray,
+        "Morph Gradient": grad,
+        "Adaptive Threshold": th,
+        "Morph close" : closed,
+        "Find Contour" : contour_img,
+    }
+
+
+# OCR & 출력
+    for variant_name, proc_img in variants.items():
+        print("-" * 60)
+        print(f"[전처리: {variant_name}]")
+
+        for psm in psm_options:
+            for oem in oem_options:
+                config = f'--oem {oem} --psm {psm} -c user_defined_dpi=300'
+                text = pytesseract.image_to_string(proc_img, lang="kor+eng", config=config)
+                print(f" OEM {oem} | PSM {psm} 결과:")
+                print(text.strip())
+        
+        
+        pass_item = ["Gray", "Morph Gradient"]
+
+        if variant_name in pass_item:
+            pass
+        else:
+            cv2.imshow(f"{name} - {variant_name}", proc_img)
+
+    print("\n")
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- ocr_demo_cd.py
+++ ocr_demo_cd.py
@@ -10,24 +10,29 @@ path_img1 = "./meme.jpg"
 path_img2 = "./resume.png"
 path_img3 = "./test.png"
 
+# 테스트할 이미지들을 리스트로 관리 (OpenCV로 불러옴)
 images = [
     ("Image 1", cv2.imread(path_img1)),
     ("Image 2", cv2.imread(path_img2)),
     ("Image 3", cv2.imread(path_img3)),
 ]
 
+# OCR 옵션
 psm_options = [6]   
 oem_options = [3]   
 
 # 전처리 함수
 def preprocess_var(gray):
     variants = {}
-    # 1. Gray
+
+    # 1. Gray (순수 그레이스케일)
     variants["Gray"] = gray
-    # 2. Otsu
-    _, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    
+    # 2. Otsu 이진화
+    _, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) # 첫 번째 값은 무시, 두 번째 값만 otsu에 저장
     variants["Otsu"] = otsu
-    # 3. Adaptive
+    
+    # 3. Adaptive Threshold
     adaptive = cv2.adaptiveThreshold(
         gray, 255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
@@ -35,6 +40,7 @@ def preprocess_var(gray):
         31, 15
     )
     variants["Adaptive"] = adaptive
+    
     # 4. CLAHE + Adaptive
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     clahe_img = clahe.apply(gray)
@@ -45,6 +51,19 @@ def preprocess_var(gray):
         31, 15
     )
     variants["CLAHE+Adaptive"] = adaptive_clahe
+    
+    # 5. Gray + CLAHE + Morphology Closing + Adaptive Threshold
+    clahe_img2 = clahe.apply(gray)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    morph = cv2.morphologyEx(clahe_img2, cv2.MORPH_CLOSE, kernel)
+    adaptive_morph = cv2.adaptiveThreshold(
+        morph, 255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY,
+        31, 15
+    )
+    variants["CLAHE+Morph+Adaptive"] = adaptive_morph
+    
     return variants
 
 # 실행
@@ -52,7 +71,19 @@ for name, img in images:
     print("=" * 70)
     print(f" {name} 결과")
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #resize
+    h, w = img.shape[:2]
+    print(f" {name} 원본 크기: {w}, {h}")
+    if w > h :
+        new_w = 1000
+        new_h = int((h / w) * 1000)
+    else:
+        new_h = 1000
+        new_w = int((w / h) * 1000)
+    
+    img_resized = cv2.resize(img, (new_w, new_h))
+
+    gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
     variants = preprocess_var(gray)
 
     for variant_name, proc_img in variants.items():
@@ -67,10 +98,10 @@ for name, img in images:
                 print(f" OEM {oem} | PSM {psm} 결과:")
                 print(text.strip())
 
-        # 이미지 창으로 띄우기
+        # 전처리된 이미지 창으로 띄우기
         cv2.imshow(f"{name} - {variant_name}", proc_img)
 
     print("\n")
 
-cv2.waitKey(0)   # 아무 키나 입력하면 닫히도록.
+cv2.waitKey(0)   # 아무 키나 입력하면 닫히도록
 cv2.destroyAllWindows()
